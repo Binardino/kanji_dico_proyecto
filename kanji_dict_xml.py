39a09e0fb3bc8b7aa4b0add2e2ee0a3c2da573b1
Robin Langlois <robin.langlois@fr.fcm.travel>
2023-01-31 14:49:02 +0100

    basic dict built - fetch jlpt, radical & stroke - need to add sub meanings & readings

--- kanji_dict_xml.py
+++ kanji_dict_xml.py
@@ -12,20 +12,40 @@ for element in root:
     if element.tag == 'character':
         #get kanji key as entry
         kanji = element.find('literal').text
-        print('kanji' : kanji)
+        print('kanji', kanji)
         
-        #get sublevel meanings
-        jlpt_level   = element.find('jlpt').text
-        stroke_count = element.find('stroke_count').text
-        radical      = element.find('radical').text
-        meaning      = element.find('meaning').text
-        reading_on   = element.find('meaning').text
-        reading_kun  = element.find('meaning').text
+        
+        if element.tag == 'misc/jlpt':
+        #not all kanji have JLPT level defined
+            jlpt_level   = element.find('misc/jlpt').text
+        else:
+            jlpt_level   = None
+        stroke_count = element.find('misc/stroke_count').text
+        #fetching radical value from classical kanji numerotation
+        radical      = element.find('radical/rad_value').text
+
+        
+        #instantiate variables
+        meanings = []
+        pronunciations = {}
+
+        #get sublevel keys
+        # for child in element:
+                        # if child.tag == 'reading_meaning':
+            #     for subchild in child:
+            #         if subchild.tag == 'rmgroup':
+            #     meaning      = child.find('meaning').text
+            #     if == r_type="ja_kun":
+            #         reading_kun  = element.find('meaning').text
+            #     elif == r_type="ja_on":
+            #         reading_on   = element.find('meaning').text
+            
         
         #Add entry to kanji_dict
         kanji_dict[kanji] = {'meanings'     : meanings,
                              'jlpt_level'   : jlpt_level,
                              'stroke_count' : stroke_count,
-                             'radicals' 	: radicals,
-
+                             'radicals' 	: radical
+                             # 'reading_kun'  : reading_kun,
+                             # 'reading_on'   : reading_on
                              }
