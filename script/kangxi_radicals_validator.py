20effcda66c8cb68cf27fa1df898a9ca788f0a0e
Binardino <langlois.robin@gmail.com>
2025-12-01 18:07:50 +0100

    basic field validator func - need docstring

--- script/kangxi_radicals_validator.py
+++ script/kangxi_radicals_validator.py
@@ -19,3 +19,12 @@ def load_radicals(path):
     with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
     
+def check_required_keys(kangxi_radical, errors):
+    for field in REQUIRED_FIELDS:
+        if field not in kangxi_radical:
+            errors.append(f"Missing field '{field}' in kangxi_radical #{kangxi_radical.get('number')}")
+        
+        else:
+            if field == "variants" and not isinstance(kangxi_radical[field], list):
+                errors.append(f"'variants' must be a list in kangxi_radical #{kangxi_radical['number']}")
+                
