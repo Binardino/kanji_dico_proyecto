af2d82026e3d450dd7ded9900c2ee71eda3b5075
Binardino <langlois.robin@gmail.com>
2026-01-01 17:38:58 +0100

    add ids_to_positioned_components func

--- script/parse_unihan_cjkvi.py
+++ script/parse_unihan_cjkvi.py
@@ -9,6 +9,18 @@ def clean_ids(ids):
     IDS_ANNONATION_REGEX = re.compile(r"\[.*?\]$")
     
     return IDS_ANNONATION_REGEX.sub("", ids)
+#%%
+def ids_to_positioned_components(parsed_ids):
+    operator = parsed_ids['operator']
+    children = parsed_ids['children']
+    
+    left_pos, right_pos = IDS_BINARY_OPERATORS[operator]
+    
+    return [
+            {'component': children[0], 'position' : left_pos},
+            {'component': children[1], 'position' : right_pos}
+            ]
+#%%
 def parse_unihan_cjkvi(path):
     IDS_OPERATORS = ("⿰", "⿱", "⿴", "⿵", "⿶","⿷", "⿸", "⿹", "⿺", "⿻")
 
